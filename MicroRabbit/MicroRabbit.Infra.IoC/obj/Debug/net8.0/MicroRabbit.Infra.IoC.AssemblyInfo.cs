27fe7adadfa938a12cb292c0a31818e51d1b1014
Dawid Wozny <dawid.wozny@argentaconsult.com>
2024-06-27 23:52:02 +0200

    another go

--- MicroRabbit/MicroRabbit.Infra.IoC/obj/Debug/net8.0/MicroRabbit.Infra.IoC.AssemblyInfo.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/obj/Debug/net8.0/MicroRabbit.Infra.IoC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroRabbit.Infra.IoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be498cfdf4de6ae29ba8d812d6b98fafbb7f7423")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4efd500dc0f42b92135b34dc1d268d26bbc7a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroRabbit.Infra.IoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroRabbit.Infra.IoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
