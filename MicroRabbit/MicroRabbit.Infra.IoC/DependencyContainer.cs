73491fd30cd1ea6311ff7a786b901a4788a3cc13
Dawid Wozny <dawid.wozny@argentaconsult.com>
2024-06-27 12:53:45 +0200

    banking start

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Services
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
